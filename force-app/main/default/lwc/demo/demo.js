962b440f1dc3748c49fcc7b418b19d91dff653d5
Bhavana02012003 <edurupakabhavana2003@gmail.com>
2025-04-22 15:29:21 +0530

    LWC Testing

--- force-app/main/default/lwc/demo/demo.js
+++ force-app/main/default/lwc/demo/demo.js
@@ -1,17 +1,17 @@
 import { LightningElement } from 'lwc';
 
 export default class Demo extends LightningElement {
-    myVar1; // ❌ `no-unused-vars`: Variable is declared but not used
+    myVa2r; // ❌ `no-unused-vars`: Variable is declared but not used
 
     connectedCallback() {
-        console.log('Component Load'); // ❌ `no-console`: `console.log()` is not allowed
+        console.log('Component Loaded'); // ❌ `no-console`: `console.log()` is not allowed
         debugger; // ❌ `no-debugger`: Debugger statement is not allowed
 
-        if (this.myVar1 == null) { // ❌ `eqeqeq`: Use `===` instead of `==`
+        if (this.myVar2 == null) { // ❌ `eqeqeq`: Use `===` instead of `==`
             console.warn("Warning: Variable is null");
         }
 
-        if (this.myVar1) console.log("No curly brackets found"); // ❌ `curly`: Requires curly brackets `{}` for if statements
+        if (this.myVar2) console.log("No curly brackets found"); // ❌ `curly`: Requires curly brackets `{}` for if statements
 
         this.testFunction();
     }
