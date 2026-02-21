9361372ab63e69f97e7127d7c61b792f1eb997eb
Bhavana02012003 <edurupakabhavana2003@gmail.com>
2026-02-21 15:25:26 +0530

    Modified both class and lwc files dataa

--- force-app/main/default/lwc/demo/demo.js
+++ force-app/main/default/lwc/demo/demo.js
@@ -4,7 +4,7 @@ export default class Demo extends LightningElement {
     myVar; // ❌ `no-unused-vars`: Variable is declared but not used
 
     connectedCallback() {
-        console.log('Component Loading Voilation dataa.....'); // ❌ `no-console`: `console.log()` is not allowed
+        console.log('Component Loading Voilation dataaa.....'); // ❌ `no-console`: `console.log()` is not allowed
         debugger; // ❌ `no-debugger`: Debugger statement is not allowed
 
         if (this.myVar === null) { // ❌ `eqeqeq`: Use `===` instead of `==`
