5a064cd958e0f3bedc2189e81aabaed854bdde44
Bhavana02012003 <godofthunder8844@gmail.com>
2026-02-27 17:01:18 +0530

    Eslint Testing4

--- eslint.config.js
+++ eslint.config.js
@@ -6,34 +6,53 @@ import importPlugin from "eslint-plugin-import";
 import jest from "eslint-plugin-jest";
 
 export default [
+  // Base JS recommended rules
   js.configs.recommended,
 
+  // -----------------------------
+  // ✅ LWC + Aura JS (controllers/helpers/components)
+  // -----------------------------
   {
-    files: ["**/{lwc,aura}/**/*.js"],
+    files: ["**/{lwc,aura}/**/*.js", "temp_scanner_files/**/*.js"],
     plugins: {
       "@lwc/lwc": lwc,
       "@salesforce/aura": aura,
       "@salesforce/lightning": lightning,
       import: importPlugin,
-      jest: jest
+      jest: jest,
     },
     languageOptions: {
       ecmaVersion: 2023,
-      sourceType: "module"
+      sourceType: "module",
     },
     rules: {
       "no-console": ["error", { allow: ["warn", "error"] }],
       "no-debugger": "error",
       "no-unused-vars": "warn",
       "no-undef": "error",
-      "eqeqeq": ["error", "always"],
-      "curly": "error",
-      "semi": ["error", "always"],
+      eqeqeq: ["error", "always"],
+      curly: "error",
+      semi: ["error", "always"],
 
       "@lwc/lwc/no-async-await-in-wire": "error",
       "@lwc/lwc/no-deprecated": "warn",
       "@lwc/lwc/no-inner-html": "error",
-      "@lwc/lwc/no-dupe-class-members": "error"
-    }
-  }
+      "@lwc/lwc/no-dupe-class-members": "error",
+    },
+  },
+
+  // -----------------------------
+  // ✅ LWC HTML templates
+  // IMPORTANT: This processor is what prevents "UnknownRule" parsing errors
+  // -----------------------------
+  {
+    files: ["**/lwc/**/*.html", "temp_scanner_files/**/*.html"],
+    plugins: {
+      "@lwc/lwc": lwc,
+    },
+    processor: lwc.processors[".html"],
+    rules: {
+      // You can keep this empty or add template rules later
+    },
+  },
 ];
