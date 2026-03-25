1704ad003dca85e61b8d3a42ed803bf4b44163d1
Shyamk-16028 <shyamk@sprintpark.com>
2026-03-25 15:46:25 +0530

    try catch test13

--- force-app/main/default/lwc/demo/demo.js
+++ force-app/main/default/lwc/demo/demo.js
@@ -22,7 +22,7 @@ export default class Demo extends LightningElement {
         try {
             // intentionally left empty
         } catch (e) {
-            console.error("Exceptions caught: " + e.message);
+            console.log("Exceptions caught: " + e.message);
         }
     }
 }
